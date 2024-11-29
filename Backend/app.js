880b232b024a4e4b2bb09cde8476731e14222629
Thibault Boucard Garda <thibault.boucardgarda@gmail.com>
2024-11-29 16:57:50 +0100

    Ajout d'une route pour que Render affiche les travaux

--- Backend/app.js
+++ Backend/app.js
@@ -1,27 +1,40 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors')
+const cors = require('cors');
 require('dotenv').config();
 const helmet = require('helmet');
-const swaggerUi = require('swagger-ui-express')
-const yaml = require('yamljs')
-const swaggerDocs = yaml.load('swagger.yaml')
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(helmet({
-      crossOriginResourcePolicy: false,
-    }));
-app.use('/images', express.static(path.join(__dirname, 'images')))
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('yamljs');
+const swaggerDocs = yaml.load('swagger.yaml');
 
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet({
+  crossOriginResourcePolicy: false,
+}));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Documentation Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Routes
 const db = require("./models");
 const userRoutes = require('./routes/user.routes');
 const categoriesRoutes = require('./routes/categories.routes');
 const worksRoutes = require('./routes/works.routes');
-db.sequelize.sync().then(()=> console.log('db is ready'));
+db.sequelize.sync().then(() => console.log('db is ready'));
+
+
+app.get('/', (req, res) => {
+  res.send('Backend is running! Access the API at /api or check /api-docs for documentation.');
+});
+
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/works', worksRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
 module.exports = app;
